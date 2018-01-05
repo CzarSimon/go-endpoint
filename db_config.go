f028107d849a688dbeb983bb07b21939ba12fac2
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-01-05 08:36:42 +0100

    Added ConnInfo struct for generic connection

--- db_config.go
+++ db_config.go
@@ -6,17 +6,26 @@ import (
 )
 
 const (
-	DB_USER_KEY = "USER"
-	DB_PWD_KEY  = "PASSWORD"
-	DB_NAME_KEY = "NAME"
+	DB_USER_KEY     = "USER"
+	DB_PWD_KEY      = "PASSWORD"
+	DB_NAME_KEY     = "NAME"
+	DB_SSL_MODE_KEY = "SSL_MODE"
 
-	DEFAULT_POSTGRES_PORT = "5432"
-	POSTGRES_PROTOCOL     = "tcp"
+	DEFAULT_POSTGRES_PORT     = "5432"
+	DEFAULT_POSTGRES_SSL_MODE = "disable"
+	POSTGRES_PROTOCOL         = "tcp"
 )
+
+// ConnInfo Information needed to connect to a database
+type ConnInfo struct {
+	DriverName string `json:"driverName"`
+	ConnStr    string `json:"connStr"`
+}
 
 // DBConfig Interface for database configuration and connection
 type DBConfig interface {
 	Connect() (*sql.DB, error)
+	ConnInfo() ConnInfo
 }
 
 // PGConfig Connection info for a postgres database
@@ -25,9 +34,10 @@ type PGConfig struct {
 	User     string `json:"username"`
 	Password string `json:"password"`
 	Database string `json:"database"`
+	SSLMode  string `json:"sslMode"`
 }
 
-// NewPGConfig
+// NewPGConfig Creates a new PG config by reading from environment variables
 func NewPGConfig(name string) PGConfig {
 	host := NewServerAddr(name)
 	host.Protocol = POSTGRES_PROTOCOL
@@ -39,15 +49,29 @@ func NewPGConfig(name string) PGConfig {
 		User:       getenv(makeKey(name, DB_USER_KEY), ""),
 		Password:   getenv(makeKey(name, DB_PWD_KEY), ""),
 		Database:   getenv(makeKey(name, DB_NAME_KEY), "postgres"),
+		SSLMode:    getenv(makeKey(name, DB_SSL_MODE_KEY), DEFAULT_POSTGRES_SSL_MODE),
+	}
+}
+
+// ConnInfo Creates and returns connection info from a PGConfig
+func (pg PGConfig) ConnInfo() ConnInfo {
+	connStr := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		pg.Host, pg.User, pg.Password, pg.Database, pg.Port, pg.SSLMode)
+	return ConnInfo{
+		DriverName: "postgres",
+		ConnStr:    connStr,
 	}
 }
 
 // Connect Attempts to connect to a postgres database. Requires a driver!
 func (pg PGConfig) Connect() (*sql.DB, error) {
-	connStr := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		pg.Host, pg.User, pg.Password, pg.Database, pg.Port)
-	db, err := sql.Open("postgres", connStr)
+	return connectDB(pg.ConnInfo())
+}
+
+// connectDB Generic connection and ping test method for a SQL db
+func connectDB(conn ConnInfo) (*sql.DB, error) {
+	db, err := sql.Open(conn.DriverName, conn.ConnStr)
 	if err != nil {
 		return db, err
 	}
